4bce1a9caa607bb962f418532822ae335f3b329f
Trigonoculus <github@trigono.culus.party>
2021-07-06 16:18:59 +0700

    Update metar command + functions

--- commands/metar.js
+++ commands/metar.js
@@ -1,6 +1,13 @@
 const Discord = require('discord.js');
-const { getLightColor } = require('../functions.js');
+const { getLightColor, findAirport, convertinHg, parseRawMetarObsTime } = require('../functions.js');
 const airportDiscovery = require('@airport-discovery/metars-tafs');
+const dayjs = require('dayjs');
+const utc = require('dayjs/plugin/utc');
+const timezone = require('dayjs/plugin/timezone');
+const duration = require('dayjs/plugin/duration');
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.extend(duration);
 
 module.exports = {
     name: 'metar',
@@ -9,25 +16,59 @@ module.exports = {
     run: async (message, args) => {
         // Search METAR
         if(!args[0]) return message.channel.send('An ICAO airport code is needed.');
+        message.channel.startTyping();
         const airport = args[0];
+        console.log(airport);
         let metar;
         try {
             metar = await airportDiscovery.metars(airport);
         } catch(err) {
             console.log(err);
+            message.channel.stopTyping();
             return message.channel.send('An error has occurred while looking up the METAR.');
         }
+        console.log(metar);
         // Check undefined
         if (typeof metar === undefined) {
-            return message.channel.send(`No data returned. Either an incorrect airport was given (${airport}) or no METARs are available at this time.`)
+            message.channel.stopTyping();
+            return message.channel.send(`No data returned. Either an incorrect airport was given (${airport}) or no METARs are available at this time.`);
         }
+        // Find airport
+        let airportData;
+        try {
+            airportData = await findAirport(airport);
+            console.log(airportData);
+        } catch(err) {
+            console.log(err);
+            message.channel.stopTyping();
+            return message.channel.send(`An error occurred while finding airport data for ${airport}.`);
+        }
+        // Wind formatting
+        let windFormat;
+        if (metar.wind.speedKt <= 3) {
+            windFormat = 'Wind calm';
+        } else {
+            windFormat = `${metar.wind.directionDegrees}° at ${metar.wind.speedKt} knots`;
+        }
+        // Get observation time + difference
+        const obsTime = parseRawMetarObsTime(metar.rawText);
+        const obsTimeDifference = new Date() - obsTime;
+        const obsTimeDifferenceMins = obsTimeDifference / 60000;
         // Make embed
         const embed = new Discord.MessageEmbed()
             .setTitle(`Weather information for ${airport}`)
-            .setTimestamp()
+            .setFooter(`Observed ${obsTimeDifferenceMins.toFixed(0)} minutes ago`)
+            .setTimestamp(obsTime)
             .setColor(getLightColor('gray'))
             .addFields(
-                // { name: search.name, value: `${adminName}${search.countryName} ${flagEmoji}`, inline: false },
+                { name: airportData.name, value: `${airportData.city}, ${airportData.country.toUpperCase()}`, inline: false },
+                { name: 'Airport Information', value: `**${dayjs(new Date()).tz(airportData.tz).format('HH:mm')}** | ${airportData.lat.toFixed(3)}, ${airportData.long.toFixed(3)} | ${airportData.altitude}ft`, inline: false },
+                { name: 'Raw METAR', value: '`' + metar.rawText + '`', inline: false },
+                { name: 'Wind', value: `${windFormat}`, inline: true },
+                { name: 'Temperature / Dew', value: `${metar.temperatureC}°C / ${metar.dewPointC}°C`, inline: true },
+                { name: 'Pressure', value: `${convertinHg(metar.altimInHg)} hPa`, inline: true },
             );
+        message.channel.send(embed);
+        message.channel.stopTyping();
     },
 };
--- functions.js
+++ functions.js
@@ -7,6 +7,16 @@ const geonames = Geonames({
     encoding: 'JSON',
 });
 const randomColor = require('randomcolor');
+const airportData = require('./data/airports.json');
+
+// init dayjs
+const dayjs = require('dayjs');
+const utc = require('dayjs/plugin/utc');
+const timezone = require('dayjs/plugin/timezone');
+const duration = require('dayjs/plugin/duration');
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.extend(duration);
 
 module.exports = {
     geonamesSearch: async function(place) {
@@ -28,5 +38,24 @@ module.exports = {
             if (lat > 0) return 'summer';
             if (lat <= 0) return 'winter';
         }
+    },
+    findAirport: function(query) {
+        if (query === null) return null;
+        if (query.length === 3) return airportData.find(ap => ap.iata === query);
+        if (query.length === 4) return airportData.find(ap => ap.icao === query);
+    },
+    convertinHg: function(qnh) { return (qnh * 33.864).toFixed(0); },
+    parseRawMetarObsTime: function(metar) {
+        // Construct observation date
+        const string = metar.split(' ')[1];
+        const now = new Date();
+        const obsDate = string.slice(0, 2);
+        const obsHour = string.slice(2, 4);
+        const obsMinute = string.slice(4, 6);
+        const curUTCMonth = dayjs(now).tz('UTC').get('month');
+        const curUTCYear = dayjs(now).tz('UTC').get('year');
+        // Finally we make date object
+        const obsTimeObj = new Date(Date.UTC(curUTCYear, curUTCMonth, obsDate, obsHour, obsMinute, 0));
+        return obsTimeObj;
     },
 };
