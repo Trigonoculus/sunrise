a054bd295f06bc7b5b903667d3408d65c2919869
Trigonoculus <github@trigono.culus.party>
2021-07-06 17:36:13 +0700

    Add sun information

--- commands/metar.js
+++ commands/metar.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const { getLightColor, findAirport, convertinHg, parseRawMetarObsTime } = require('../functions.js');
+const { getLightColor, findAirport, convertinHg, parseRawMetarObsTime, getSeason } = require('../functions.js');
 const airportDiscovery = require('@airport-discovery/metars-tafs');
 const dayjs = require('dayjs');
 const utc = require('dayjs/plugin/utc');
@@ -8,6 +8,12 @@ const duration = require('dayjs/plugin/duration');
 dayjs.extend(utc);
 dayjs.extend(timezone);
 dayjs.extend(duration);
+const SunCalc = require('suncalc');
+
+function formatSunString(date, tz) {
+    const string = isNaN(date) ? '--:--' : dayjs(date).tz(tz).format('HH:mm');
+    return string;
+}
 
 module.exports = {
     name: 'metar',
@@ -29,9 +35,9 @@ module.exports = {
         }
         console.log(metar);
         // Check undefined
-        if (typeof metar === undefined) {
+        if (typeof metar === 'undefined') {
             message.channel.stopTyping();
-            return message.channel.send(`No data returned. Either an incorrect airport was given (${airport}) or no METARs are available at this time.`);
+            return message.channel.send(`No data found. Either an incorrect airport was given (${airport}) or no METARs are available at this time.`);
         }
         // Find airport
         let airportData;
@@ -54,15 +60,39 @@ module.exports = {
         const obsTime = parseRawMetarObsTime(metar.rawText);
         const obsTimeDifference = new Date() - obsTime;
         const obsTimeDifferenceMins = obsTimeDifference / 60000;
+        // Sun calculation
+        let sun, polarStatus;
+        const tz = airportData.tz;
+        try {
+            sun = SunCalc.getTimes(new Date(), airportData.lat, airportData.long);
+        } catch (err) {
+            message.channel.stopTyping();
+            console.log(err);
+            return message.channel.send('An error has occurred.');
+        }
+        if(isNaN(sun.sunrise) || isNaN(sun.sunset)) {
+            const season = getSeason(airportData.lat, new Date());
+            if (season === 'winter') polarStatus = 'down all day';
+            if (season === 'summer') polarStatus = 'up all day';
+        }
+        // Sun string formatting
+        const dawn = formatSunString(sun.dawn, tz);
+        const sunrise = formatSunString(sun.sunrise, tz);
+        const sunset = formatSunString(sun.sunset, tz);
+        const dusk = formatSunString(sun.dusk, tz);
+        const sunString = `${dawn} ↗️ ${sunrise} ☀️ ${sunset} ↘️ ${dusk}${(polarStatus) ? ' [' + polarStatus + ']' : ''}`;
         // Make embed
         const embed = new Discord.MessageEmbed()
             .setTitle(`Weather information for ${airport}`)
             .setFooter(`Observed ${obsTimeDifferenceMins.toFixed(0)} minutes ago`)
             .setTimestamp(obsTime)
-            .setColor(getLightColor('gray'))
+            .setColor((obsTimeDifferenceMins <= 60) ? '#A7DB42' : '#FF7F7F')
             .addFields(
                 { name: airportData.name, value: `${airportData.city}, ${airportData.country.toUpperCase()}`, inline: false },
-                { name: 'Airport Information', value: `**${dayjs(new Date()).tz(airportData.tz).format('HH:mm')}** | ${airportData.lat.toFixed(3)}, ${airportData.long.toFixed(3)} | ${airportData.altitude}ft`, inline: false },
+                { name: 'Airport information', value: sunString, inline: false },
+                { name: 'Local time', value: dayjs(new Date()).tz(airportData.tz).format('HH:mm Z'), inline: true },
+                { name: 'Elevation', value: `${airportData.altitude}ft`, inline: true },
+                { name: 'Coordinates', value: `${airportData.lat.toFixed(3)}, ${airportData.long.toFixed(3)}`, inline: true },
                 { name: 'Raw METAR', value: '`' + metar.rawText + '`', inline: false },
                 { name: 'Wind', value: `${windFormat}`, inline: true },
                 { name: 'Temperature / Dew', value: `${metar.temperatureC}°C / ${metar.dewPointC}°C`, inline: true },
